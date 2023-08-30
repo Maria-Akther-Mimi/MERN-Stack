a773e1a16f1f6b3ab94153af90b0eb9d6312ab95
Most. Maria Akther Mimi <134642219+Maria-Akther-Mimi@users.noreply.github.com>
2023-08-30 22:58:02 +0600

    Add files via upload

---
+++ Module5/src/controllers/DemoController.js
@@ -0,0 +1,21 @@
+exports.demo1=(req,res)=>{
+    res.send('This is Home Page')
+}
+
+exports.demo2=(req,res)=>{
+    res.send('This is About Page')
+}
+
+
+
+exports.demo3=(req,res)=>{
+    res.send('This is Contact Page')
+}
+
+
+exports.demo4=(req,res)=>{
+    res.send('File "demo.txt" created and "hello world" written')
+}
+
+
+
